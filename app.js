53423071b88714cd4ee1f96d9151e83e7e2c9e37
smithynb <smithynb@oregonstate.edu>
2026-02-07 15:40:30 -0800

    add new route handlers for books, coupons, orders, and members

--- app.js
+++ app.js
@@ -2,52 +2,91 @@
 // ########## SETUP
 
 // Express
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const PORT = 55902;
 
 // Database
-const db = require('./database/db-connector');
+const db = require("./database/db-connector");
 
 // Handlebars
-const { engine } = require('express-handlebars'); // Import express-handlebars engine
-app.engine('.hbs', engine({ extname: '.hbs' })); // Create instance of handlebars
-app.set('view engine', '.hbs'); // Use handlebars engine for *.hbs files.
+const { engine } = require("express-handlebars"); // Import express-handlebars engine
+app.engine(".hbs", engine({ extname: ".hbs" })); // Create instance of handlebars
+app.set("view engine", ".hbs"); // Use handlebars engine for *.hbs files.
 
 // ########################################
 // ########## ROUTE HANDLERS
 
 // READ ROUTES
-app.get('/', async function (req, res) {
+app.get("/", async function (req, res) {
     try {
-        res.render('home'); // Render the home.hbs file
+        res.render("home"); // Render the home.hbs file
     } catch (error) {
-        console.error('Error rendering page:', error);
+        console.error("Error rendering page:", error);
         // Send a generic error message to the browser
-        res.status(500).send('An error occurred while rendering the page.');
+        res.status(500).send("An error occurred while rendering the page.");
     }
 });
 
-app.get('/bsg-people', async function (req, res) {
+app.get("/bsg-people", async function (req, res) {
     try {
         // Create and execute our queries
         const query1 = `SELECT memberID, firstName, lastName, email, phoneNumber FROM members ORDER BY lastName ASC;`;
-        const query2 = 'SELECT * FROM members;';
+        const query2 = "SELECT * FROM members;";
         const [orders] = await db.query(query1);
         const [books] = await db.query(query2);
 
-
-        res.render('members', { orders: orders, books: books });
+        res.render("members", { orders: orders, books: books });
     } catch (error) {
-        console.error('Error executing queries:', error);
+        console.error("Error executing queries:", error);
         // Send a generic error message to the browser
         res.status(500).send(
-            'An error occurred while executing the database queries.'
+            "An error occurred while executing the database queries.",
         );
+    }
+});
+
+app.get("/books", async function (req, res) {
+    try {
+        res.render("books"); // Render the books.hbs file
+    } catch (error) {
+        console.error("Error rendering page:", error);
+        // Send a generic error message to the browser
+        res.status(500).send("An error occurred while rendering the page.");
+    }
+});
+
+app.get("/coupons", async function (req, res) {
+    try {
+        res.render("coupons"); // Render the coupons.hbs file
+    } catch (error) {
+        console.error("Error rendering page:", error);
+        // Send a generic error message to the browser
+        res.status(500).send("An error occurred while rendering the page.");
+    }
+});
+
+app.get("/orders", async function (req, res) {
+    try {
+        res.render("orders"); // Render the orders.hbs file
+    } catch (error) {
+        console.error("Error rendering page:", error);
+        // Send a generic error message to the browser
+        res.status(500).send("An error occurred while rendering the page.");
+    }
+});
+
+app.get("/members", async function (req, res) {
+    try {
+        res.render("members"); // Render the members.hbs file
+    } catch (error) {
+        console.error("Error rendering page:", error);
+        // Send a generic error message to the browser
+        res.status(500).send("An error occurred while rendering the page.");
     }
 });
 
@@ -56,9 +95,9 @@ app.get('/bsg-people', async function (req, res) {
 
 app.listen(PORT, function () {
     console.log(
-        'Express started on http://localhost:' +
+        "Express started on http://localhost:" +
             PORT +
-            '; press Ctrl-C to terminate.'
+            "; press Ctrl-C to terminate.",
     );
 });
 
