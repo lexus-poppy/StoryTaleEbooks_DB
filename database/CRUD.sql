03c40e1544e7c08561954331930d16506a2f89b2
Erika <browner5@oregonstate.edu>
2026-02-12 09:41:27 -0800

    Implement CRUD operations for Coupons and Orders
    Added CRUD operations for Coupons, Orders, and BooksAndOrders with appropriate SQL statements.

--- database/CRUD.sql
+++ database/CRUD.sql
@@ -40,10 +40,82 @@ WHERE ISBN = :isbnInput
 DELETE FROM Books
 WHERE ISBN = :isbnInput;
 
+-- ########################################
+-- ######     COUPONS CRUD          #######
+-- ########################################
+
+-- CREATE
+INSERT INTO Coupons (couponDiscount, expirationDate)
+VALUES (:discountInput, :expiryInput)
+
+-- READ
+SELECT *
+FROM Coupons;
+
+-- UPDATE
+UPDATE Coupons
+SET couponDiscount = :discountInput, expirationDate = :expiryInput
+WHERE couponID = :couponID;
+
+-- DELETE
+DELETE FROM Coupons
+WHERE couponID = :couponID;
+
+-- ########################################
+-- ######     ORDERS CRUD           #######
+-- ########################################
+
+-- CREATE
+INSERT INTO Orders (totalPrice, couponID, memberID)
+VALUES (:totalPriceInput, :couponID_from_dropdown, :memberID_from_dropdown);
+
+-- READ
+SELECT Orders.orderID, Members.firstName, Members.lastName, Coupons.couponDiscount, Orders.totalPrice
+FROM Orders
+JOIN Members ON Orders.memberID = Members.memberID
+LEFT JOIN Coupons ON Orders.couponID = Coupons.couponID;
+
+-- UPDATE
+UPDATE Orders
+SET totalPrice = :totalPriceInput
+WHERE orderID = :orderID;
+
+-- DELETE
+DELETE FROM BooksAndOrders
+WHERE orderID = :orderID_from_frontend;
+
+DELETE FROM Orders
+WHERE orderID = :orderID_from_frontend;
+
+-- ########################################
+-- ######    BOOKS AND ORDERS CRUD  #######
+-- ########################################
+
+-- CREATE
+INSERT INTO BooksAndOrders (ISBN, orderID, QTY)
+VALUES (:isbn_from_dropdown, :orderID_from_frontend, :qtyInput);
+
+-- READ
+SELECT BooksAndOrders.booksAndOrdersID, BooksAndOrders.orderID, Books.title, Books.ISBN, BooksAndOrders.QTY, Orders.totalPrice
+FROM BooksAndOrders
+JOIN Books ON BooksAndOrders.ISBN = Books.ISBN
+JOIN Orders ON BooksAndOrders.orderID = Orders.orderID;
+
+-- UPDATE
+UPDATE BooksAndOrders
+SET QTY = :qtyInput
+WHERE booksAndOrdersID = :booksAndOrdersID;
+
+-- DELETE
+DELETE FROM BooksAndOrders
+WHERE booksAndOrdersID = :booksAndOrdersID;
+
+
 
 --        Citation for use of AI Tools:
 --        Date: 02/11/2026
 --        Prompts used to generate CRUD.sql content
+--        Add Joins to Orders and BooksAndOrders that work with the Handlebars files
 --        Fix this CRUD.sql file so that all of the variable names match all of the variables in the handlebars files and app.js file
 --        AI Source URL: https://github.com/copilot?utm_campaign=copilot-brand&utm_medium=sem&utm_source=google&ocid=AIDcmmh2h80ugd_SEM__k_Cj0KCQiA7rDMBhCjARIsAGDBuEBD7rM6foqE_BsmLK2EynZLqQYBBDbyXJEvXpFu7Q90l-IZq94r6IoaAsEbEALw_wcB_k_
 --        Links to an external site.
